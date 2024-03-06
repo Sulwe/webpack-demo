c1046a1f3309fe5d2a3025a5e85b74eaca8cd107
Sulwe <cypriansulwe.cs@gmail.com>
2024-03-06 12:12:31 +0300

    add styles - asset management using webpack

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-//import _ from 'lodash';
+import _ from 'lodash';
+import './style.css';
 
-import myName from './myName';
+//import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
 
     //Lodash, now imported by this script
-    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.textContent = myName('Codify');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
 }
