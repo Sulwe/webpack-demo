2b3888ca2b2af3a136a6e546b44ae4655518bb1e
Sulwe <cypriansulwe.cs@gmail.com>
2025-02-14 19:52:18 +0300

    set up webpack and configure plugins and modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import "./styles.css";
+import myIcon from "./icon.png";
+
 import {greeting} from "./greeting.js";
 
 console.log(greeting);
+
+const image = document.createElement('img');
+image.src = myIcon;
+
+document.body.appendChild(image);
